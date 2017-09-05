957e5b7773e29a75d62002e0eaf59607a78b4659
Roberto Mier Escandon <rmescandon@gmail.com>
2017-09-05 11:28:19 +0200

    updtaed go version

--- config/config.go
+++ config/config.go
@@ -28,7 +28,7 @@ import (
 )
 
 // Set the application version from a constant
-const version = "1.6.0"
+const version = "2.0-0"
 
 // Settings defines the parsed config file settings.
 type Settings struct {
