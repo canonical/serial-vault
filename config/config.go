9b0e5b1a458b089ec71078bdc6b0270448898b84
James Jesudason <james.jesudason@gmail.com>
2018-03-02 11:56:07 +0000

    Update the version to 2.1-2

--- config/config.go
+++ config/config.go
@@ -28,7 +28,7 @@ import (
 )
 
 // Set the application version from a constant
-const version = "2.1-1"
+const version = "2.1-2"
 
 // Settings defines the parsed config file settings.
 type Settings struct {
