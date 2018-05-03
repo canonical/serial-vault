0c6a330f14daccfd5dc6c80c48d1d93cf5013321
James Jesudason <james.jesudason@gmail.com>
2018-05-03 13:55:47 +0100

    Increase version to 2.3-0

--- config/config.go
+++ config/config.go
@@ -28,7 +28,7 @@ import (
 )
 
 // Set the application version from a constant
-const version = "2.2-6"
+const version = "2.3-0"
 
 // Settings defines the parsed config file settings.
 type Settings struct {
