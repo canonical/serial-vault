646f2ce31dbd20bf503fc14895e26b0f07e0b6b6
James Jesudason <james.jesudason@gmail.com>
2018-05-09 13:41:29 +0100

    Version update to 2.3-1

--- config/config.go
+++ config/config.go
@@ -28,7 +28,7 @@ import (
 )
 
 // Set the application version from a constant
-const version = "2.3-0"
+const version = "2.3-1"
 
 // Settings defines the parsed config file settings.
 type Settings struct {
