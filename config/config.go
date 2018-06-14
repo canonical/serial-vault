26d19e9b280a3ac3f7714e61a6ae87cb084e0aba
James Jesudason <james.jesudason@gmail.com>
2018-06-14 11:07:45 +0100

    Update the version to 2.4-1

--- config/config.go
+++ config/config.go
@@ -28,7 +28,7 @@ import (
 )
 
 // Set the application version from a constant
-const version = "2.4-0"
+const version = "2.4-1"
 
 // Settings defines the parsed config file settings.
 type Settings struct {
