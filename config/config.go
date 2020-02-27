2ff2bf0a5d8c010ecaae2eb9d762ba8731e41507
Igor Komlew <igor.komlew@canonical.com>
2020-02-27 10:11:41 +0100

    Fixed golang build (#270)
    * fixed golang build
    
    * added missing package
    
    * bump version

--- config/config.go
+++ config/config.go
@@ -29,7 +29,7 @@ import (
 )
 
 // Set the application version from a constant
-const version = "2.4-15"
+const version = "2.4-16"
 
 // Settings defines the parsed config file settings.
 type Settings struct {
