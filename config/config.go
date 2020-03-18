a268408cdecf57e3a29a23b867d18a8b2db3d1d8
Igor Komlew <igor.komlew@canonical.com>
2020-03-18 18:41:44 +0100

    Bump the version
    Update the version to 2.4-17

--- config/config.go
+++ config/config.go
@@ -29,7 +29,7 @@ import (
 )
 
 // Set the application version from a constant
-const version = "2.4-16"
+const version = "2.4-17"
 
 // Settings defines the parsed config file settings.
 type Settings struct {
