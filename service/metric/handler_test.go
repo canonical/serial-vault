dd4e1bc59696a2d5652d91dc3fac4de4b4824811
Igor Komlew <igor.komlew@canonical.com>
2020-03-20 14:18:17 +0100

    added basic metric test

---
+++ service/metric/handler_test.go
@@ -0,0 +1,24 @@
+package metric_test
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"gopkg.in/check.v1"
+
+	"github.com/CanonicalLtd/serial-vault/service/metric"
+)
+
+type MetricSuite struct{}
+
+func TestMetricSuite(t *testing.T) { check.TestingT(t) }
+
+var _ = check.Suite(&MetricSuite{})
+
+func (s *MetricSuite) TestMetricHandler(c *check.C) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	metric.NewServer(metric.SigningMode).ServeHTTP(w, r)
+	c.Assert(w.Code, check.Equals, 200)
+	c.Assert(w.Header().Get("Content-Type"), check.Equals, "text/plain")
+}
